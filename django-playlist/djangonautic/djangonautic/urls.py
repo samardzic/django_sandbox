f3d4196936733c7b04fb29aa606af81cc7b323c9
nsamardzic <nenad.samardzic@gmail.com>
2019-05-07 21:34:33 +0200

    home

--- django-playlist/djangonautic/djangonautic/urls.py
+++ django-playlist/djangonautic/djangonautic/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
+    path('home/', views.home),
 ]
--- django-playlist/djangonautic/djangonautic/views.py
+++ django-playlist/djangonautic/djangonautic/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def about(request):
     return HttpResponse('about')
+
+def home(request):
+    return HttpResponse('homepage')
